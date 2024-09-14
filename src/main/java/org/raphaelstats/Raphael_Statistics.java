52224ce6d974f102af231585537036e6266ccd4e
Endferoma <cloudworkend@gmail.com>
2024-09-14 12:58:31 +0200

    Initial commit

---
+++ src/main/java/org/raphaelstats/Raphael_Statistics.java
@@ -0,0 +1,17 @@
+package org.raphaelstats;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Raphael_Statistics extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
